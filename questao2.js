384958cdb360e1b65e1a2a4cab7865b1afacff76
Ana Eloize <anaeloizea@gmail.com>
2024-04-10 10:31:47 -0400

    add exercicio questao2

---
+++ questao2.js
@@ -0,0 +1,14 @@
+/*  
+Questão 2: Crie um código em JavaScript para adicionar um elemento no final de
+um vetor.
+*/
+
+// Definindo um vetor inicial
+var  vetor = [1, 2, 3, 4, 5];
+
+// Adicionando um elemento ao final do vetor
+var novoElemento = 6;
+vetor.push(novoElemento);
+
+// Imprimindo o vetor atualizado
+console.log(vetor);
