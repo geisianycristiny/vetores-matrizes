10fd947a21b560cec90e4163cb0bea3b93b919e7
Ana Eloize <anaeloizea@gmail.com>
2024-04-10 10:25:33 -0400

    add exercicio questao1

---
+++ questao1.js
@@ -0,0 +1,10 @@
+/* 
+Questão 1: Crie um vetor em JavaScript contendo os números de 1 a 5 e imprima-o
+na tela.
+*/
+
+// Criar um vetor contendo os números de 1 a 5
+var numeros = [1, 2, 3, 4, 5];
+
+// Imprimir o vetor na tela
+console.log(numeros);
